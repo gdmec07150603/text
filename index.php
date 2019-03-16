9f60202b42b5553221a3aec23ee5a8201c17e1a3
ccg <1019387889@qq.com>
2019-03-16 17:22:35 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+//字符串去重复着 “a,s,c,d,a,a,s,c” 
+public function unique($str){
+      $arr = explode(',',$str);
+      $arr = array_unique($arr);
+      $data = implode(',',$arr);
+      $data = trim($data,',');
+      return $data;
+ }
+ var_dump(unique('a,s,c,d,a,a,s,c'));// 输出 "a,s,c,d"
